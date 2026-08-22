734c9ec4e81a06bf177422b3513967937d313928
Alberto P <alpegom2001@gmail.com>
2026-08-23 00:03:26 +0200

    bug fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel";
+
 // Dominio canónico del sitio (usado también en Layout.astro y robots.txt).
 export default defineConfig({
   site: "https://albertoapps.info",
   integrations: [sitemap()],
+  adapter: vercel(),
 });
